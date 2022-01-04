4b97c11d16396d32082b6f4f62d5c2e73ddbdb1e
Lighto2stylo <53895514+Lighto2stylo@users.noreply.github.com>
2022-01-04 12:29:48 +0100

    The filter Method

--- script.js
+++ script.js
@@ -255,3 +255,23 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // });
 
 // console.log(movementsDescriptions);
+
+/////////////////////////////////////////////////
+
+// //THE FILTER METHOD
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+
+// console.log(movements);
+// console.log(deposits);
+
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// console.log(depositsFor);
+
+// const withdrawals = movements.filter(function (mov) {
+//   return mov < 0;
+// });
+
+// console.log(withdrawals);
