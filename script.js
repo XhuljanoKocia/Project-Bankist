703d8713e3648d9b265c9fe5d8c0257f16821aaf
Lighto2stylo <53895514+Lighto2stylo@users.noreply.github.com>
2022-01-04 15:32:09 +0100

    The reduce Method

--- script.js
+++ script.js
@@ -81,6 +81,13 @@ displayMovements(account1.movements);
 
 //console.log(containerMovements.innerHTML);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+
+calcDisplayBalance(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -275,3 +282,25 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // });
 
 // console.log(withdrawals);
+
+/////////////////////////////////////////////////
+
+// //THE REDUCE METHOD
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+
+// console.log(balance);
+
+// let balance2 = 0;
+// for (const mov of movements) balance2 += mov;
+// console.log(balance2);
+
+// //Maximum value of the array
+// const max = movements.reduce((acc, mov) => {
+//   if (acc > mov) return acc;
+//   else return mov;
+// }, movements[0]);
+
+// console.log(max);
