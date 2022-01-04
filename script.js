9f4930a74dc19c7a4779a3cea31b2f8643ddc9da
Lighto2stylo <53895514+Lighto2stylo@users.noreply.github.com>
2022-01-04 12:00:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -214,4 +214,29 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-//THE MAP METHOD
+// //THE MAP METHOD
+// const euroToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * euroToUsd;
+// });
+
+// //Converting to arrow function
+// //const movementsUSD = movements.map(mov => mov * euroToUsd);
+
+// console.log(movements);
+// console.log(movementsUSD);
+
+// const movementsUSDfor = [];
+// for (const mov of movements) movementsUSDfor.push(mov * euroToUsd);
+// console.log(movementsUSDfor);
+
+// const movementsDescriptions = movements.map((mov, i, arr) => {
+//   if (mov > 0) {
+//     return `Movement ${i + 1}: You deposited ${mov}`;
+//   } else {
+//     return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+//   }
+// });
+
+// console.log(movementsDescriptions);
