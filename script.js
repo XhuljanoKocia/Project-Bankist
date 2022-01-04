afdf9d231d6588ca9ad29762b2b81cbc3834e4b2
Lighto2stylo <53895514+Lighto2stylo@users.noreply.github.com>
2022-01-04 16:50:51 +0100

    Testing #2

--- script.js
+++ script.js
@@ -304,3 +304,21 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // }, movements[0]);
 
 // console.log(max);
+
+/////////////////////////////////////////////////
+
+// //CODING CHALLENGE #2
+
+// const calcAverageHumanAge = function (ages) {
+//   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//   console.log(humanAges);
+//   const adults = humanAges.filter(age => age >= 18);
+//   console.log(adults);
+
+//   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+//   return average;
+// };
+
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
