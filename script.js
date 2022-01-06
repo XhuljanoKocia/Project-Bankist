575f0a297665959d496c5345d77b016f251af924
Lighto2stylo <53895514+Lighto2stylo@users.noreply.github.com>
2022-01-06 10:16:51 +0100

    Implementing Transfers

--- script.js
+++ script.js
@@ -77,9 +77,9 @@ const displayMovements = function (movements) {
   });
 };
 
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance}€`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
 };
 
 const calcDisplaySummary = function (acc) {
@@ -120,6 +120,15 @@ createUsernames(accounts);
 //Event handler
 let currentAccount;
 
+const updateUI = function (acc) {
+  //Display movements
+  displayMovements(currentAccount.movements);
+  //Display balance
+  calcDisplayBalance(currentAccount);
+  //Display summary
+  calcDisplaySummary(currentAccount);
+};
+
 btnLogin.addEventListener('click', function (e) {
   //Prevent form from submitting
   e.preventDefault();
@@ -138,12 +147,31 @@ btnLogin.addEventListener('click', function (e) {
     //Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
-    //Display movements
-    displayMovements(currentAccount.movements);
-    //Display balance
-    calcDisplayBalance(currentAccount.movements);
-    //Display summary
-    calcDisplaySummary(currentAccount);
+    //Update UI
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    //Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    //Update UI
+    updateUI(currentAccount);
   }
 });
 
@@ -409,3 +437,5 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(account);
 
 /////////////////////////////////////////////////
+
+//THE FINDINDEX METHOD
